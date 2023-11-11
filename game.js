0c622d090537e9ceee2320b53c09712c148ee1e7
Batoul Hamdan <batoul.hamdan19@gmail.com>
2023-11-11 21:35:56 +0200

    Applying audio for btns

---
+++ game.js
@@ -0,0 +1,30 @@
+// Declaring btns
+let green_btn = document.getElementById('green')
+let red_btn = document.getElementById('red')
+let yellow_btn = document.getElementById('yellow')
+let blue_btn = document.getElementById('blue')
+
+// creating audio
+let audio = document.createElement('audio')
+
+// Applying audio for each btn color
+green_btn.addEventListener('click', function() {
+    audio.src = './sounds/green.mp3'
+    audio.play()
+})
+
+red_btn.addEventListener('click', function() {
+    audio.src = './sounds/red.mp3'
+    audio.play()
+})
+
+yellow_btn.addEventListener('click', function() {
+    audio.src = './sounds/yellow.mp3'
+    audio.play()
+})
+
+blue_btn.addEventListener('click', function() {
+    audio.src = './sounds/blue.mp3'
+    audio.play()
+})
+
